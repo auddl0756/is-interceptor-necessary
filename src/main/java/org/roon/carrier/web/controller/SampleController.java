7c9b33b69b548111d36086d8f7e63f956bb4c746
auddl0756 <cjddl789123@naver.com>
2022-01-09 18:13:40 +0900

    인터셉터에서 handler 정보 얻고 로깅하기

--- src/main/java/org/roon/carrier/web/controller/SampleController.java
+++ src/main/java/org/roon/carrier/web/controller/SampleController.java
@@ -1,17 +1,21 @@
 package org.roon.carrier.web.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+
+@Log4j2
 @RequestMapping("/sample")
 @Controller
 public class SampleController {
 
     @ResponseBody
     @GetMapping
-    public String hello() {
+    public String hello(HttpServletResponse response) {
         return "hello";
     }
 
--- src/main/java/org/roon/carrier/web/interceptor/LogInterceptor.java
+++ src/main/java/org/roon/carrier/web/interceptor/LogInterceptor.java
@@ -1,6 +1,8 @@
 package org.roon.carrier.web.interceptor;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.catalina.webresources.war.Handler;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,8 +15,8 @@ public class LogInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 적용 시점
-            // handler mapping이 어떤 hanlder를 통해 요청을 처리할 지 결정한 후
-            // 하지만 hanlder adapter가 요청을 실제로 처리하기는 전
+        // handler mapping이 어떤 hanlder를 통해 요청을 처리할 지 결정한 후
+        // 하지만 hanlder adapter가 요청을 실제로 처리하기는 전
 
         log.info("auth type? : " + request.getAuthType());
 
@@ -28,13 +30,14 @@ public class LogInterceptor implements HandlerInterceptor {
         // false -> 이 인터셉터에서 응답을 처리했다고 dispatcher servlet에게 알림
         //          http error를 리턴하거나, custom response를 쓴다.(printWriter였던가?)
 
-        return false;
-//        return true;
+//        return false;
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        log.info("이 메소드가 처리했음 : " + handlerMethod.getShortLogMessage());
     }
 
     @Override
