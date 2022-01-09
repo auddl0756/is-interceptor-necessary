2304585ef3c78d26facacec28f536c951202433e
auddl0756 <cjddl789123@naver.com>
2022-01-09 15:47:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 의문점들
+- interceptor가 정확히 뭔지
+- interceptor가 왜 필요한지
+- interceptor가 꼭 필요한지
+- interceptor 사용 예시는?
+- 필터, 컨트롤러랑 차이는?
+- 스프링의 인터셉터랑 서블렛의 인터셉터랑 똑같은건지
+- 보안과 관련은? 
+
+
---
+++ src/main/java/org/roon/carrier/CarrierApplication.java
@@ -0,0 +1,13 @@
+package org.roon.carrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrierApplication.class, args);
+    }
+
+}
